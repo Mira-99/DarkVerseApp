72ae95ff4230ba52147ab429b5d583b4ef4bc49e
Mira-99 <abdisharid1998@gmail.com>
2025-07-22 19:53:42 +0300

    Create UserRank.kt

---
+++ app/src/main/java/com/darkverse/app/models/UserRank.kt
@@ -0,0 +1,21 @@
+package com.darkverse.app.models
+
+enum class UserRank(
+    val displayName: String,
+    val color: String,
+    val minLevel: Int
+) {
+    NEWBIE("مبتدئ", "#8E8E93", 1),
+    OTAKU("أوتاكو", "#007AFF", 5),
+    SENPAI("سينباي", "#34C759", 15),
+    SENSEI("سينسي", "#FF9500", 30),
+    LEGEND("أسطورة", "#FF3B30", 50),
+    SHADOW_MASTER("سيد الظلال", "#5856D6", 75),
+    DARK_LORD("سيد الظلام", "#000000", 100);
+
+    companion object {
+        fun fromName(name: String): UserRank {
+            return values().find { it.name == name } ?: NEWBIE
+        }
+    }
+}
