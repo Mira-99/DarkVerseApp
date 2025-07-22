5641198aab9fe510a7f1c3f544ed46b23128d4e9
Mira-99 <abdisharid1998@gmail.com>
2025-07-22 20:06:22 +0300

    Update UserRank.kt

--- app/src/main/java/com/darkverse/app/models/UserRank.kt
+++ app/src/main/java/com/darkverse/app/models/UserRank.kt
@@ -11,7 +11,8 @@ enum class UserRank(
     SENSEI("سينسي", "#FF9500", 30),
     LEGEND("أسطورة", "#FF3B30", 50),
     SHADOW_MASTER("سيد الظلال", "#5856D6", 75),
-    DARK_LORD("سيد الظلام", "#000000", 100);
+    DARK_LORD("سيد الظلام", "#000000", 100),
+    ADMIN("مشرف", "#FF0000", 999);
 
     companion object {
         fun fromName(name: String): UserRank {
