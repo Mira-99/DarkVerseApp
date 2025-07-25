597fb06b6f3e192be3ba335fac7a246078f7e214
Mira-99 <abdisharid1998@gmail.com>
2025-07-25 15:51:41 +0300

    Update Post.kt

--- app/src/main/java/com/darkverse/app/models/Post.kt
+++ app/src/main/java/com/darkverse/app/models/Post.kt
@@ -1,10 +1,8 @@
 package com.darkverse.app.models
 
 data class Post(
-    val username: String,
-    val caption: String,
-    val timestamp: String,
-    val userRank: String,
-    val userCountry: String,
-    val userGender: String
+    val username: String = "",
+    val content: String = "",
+    val imageUrl: String = "",
+    val timestamp: Long = 0L
 )
